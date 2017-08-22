86c04dcf52eec56f5435b9180c3f6804f588c04a
Eli <elicsiegel@gmail.com>
2017-08-22 11:03:56 -0400

    Bootstrapped current user

--- frontend/medio.jsx
+++ frontend/medio.jsx
@@ -7,12 +7,21 @@ import configureStore from './store/store';
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.signup = signup; 
-  window.login = login; 
-  window.logout = logout;
-  const store = configureStore();
-  window.store = store; 
-  window.dispatch = store.dispatch;
+  // window.signup = signup; 
+  // window.login = login; 
+  // window.logout = logout;
+  // const store = configureStore();
+  // window.store = store; 
+  // window.dispatch = store.dispatch;
+  let store;
+  
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
 
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
