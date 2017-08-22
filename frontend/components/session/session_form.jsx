49519ae73b9077631b4343e82dc490dccc42b896
Eli <elicsiegel@gmail.com>
2017-08-22 10:56:53 -0400

    Create functioning login and signout form

---
+++ frontend/components/session/session_form.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
+
+class SessionForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+    this.updateInput = this.updateInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  updateInput(type) {
+    return (e) => this.setState({ [type]: e.currentTarget.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const user = Object.assign({}, this.state);
+    this.props.processForm(user);
+  }
+
+
+  render() {
+    const form = (
+        <form onSubmit={ this.handleSubmit }>
+          <input type="text" id="username" onChange={ this.updateInput('username') }></input>
+          <input type="text" id="password" onChange={ this.updateInput('password') }></input>
+          <button>Submit</button>
+        </form>
+    );
+    const errors = this.props.errors.map((error) => <li>{ error }</li>);
+
+    if (this.props.loggedIn){
+      return <Redirect to="/" />;
+    }
+
+    if(this.props.formType ===  '/login'){
+      return(
+        <div>
+          <Link to='/signup'>Sign Up</Link>
+          <h2>Login</h2>
+          { errors }
+          { form }
+        </div>
+      );
+    }
+    else{
+      return(
+        <div>
+          <Link to='/login'>Log In</Link>
+          <h2>Signup</h2>
+          { errors }
+          { form }
+        </div>
+      );
+    }
+  }
+}
+
+export default SessionForm;
---
+++ frontend/components/session/session_form_container.js
@@ -0,0 +1,27 @@
+import { connect } from 'react-redux';
+import SessionForm from './session_form';
+import { login, signup } from '../../actions/session_actions';
+import { withRouter } from 'react-router-dom';
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    loggedIn: Boolean(state.session.currentUser),
+    errors: state.session.errors,
+    formType: ownProps.location.pathname
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    processForm: (user) => {
+      if(ownProps.location.pathname ===  'login'){
+        return dispatch(login(user));
+      }
+      else{
+        return dispatch(signup(user));
+      }
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SessionForm);
