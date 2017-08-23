5b01a230abe4c288a531bd2b74dc9e20e1fe79f4
Eli <elicsiegel@gmail.com>
2017-08-23 12:28:20 -0400

    Add links to show page on each story index item

--- frontend/components/stories/stories_index_item.jsx
+++ frontend/components/stories/stories_index_item.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class StoriesIndexItem extends React.Component {
 
   render() {
+    // debugger
     const formBody = this.props.story.body.slice(0, 100) + "..."
+    const link = `/stories/${this.props.story.id}`
     return (
       <div className="storiesIndexItem">
-        <h4>{this.props.story.title}</h4>
+        <Link to={link}><h4>{this.props.story.title}</h4></Link>
         <p>{formBody}</p>
       </div>
     );
