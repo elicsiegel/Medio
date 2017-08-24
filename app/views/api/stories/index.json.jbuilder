906f227d7acec2fe010d56a5433536fa3019c20a
Eli <elicsiegel@gmail.com>
2017-08-24 14:14:11 -0400

    Style index page items

--- app/views/api/stories/index.json.jbuilder
+++ app/views/api/stories/index.json.jbuilder
@@ -3,6 +3,7 @@
     json.id story.id
     json.title story.title
     json.body story.body
+    json.story_img_url asset_path(story.image.url)
     json.author story.author, :id, :username
   end
 end
--- frontend/components/stories/stories_index_item.jsx
+++ frontend/components/stories/stories_index_item.jsx
@@ -9,8 +9,11 @@ class StoriesIndexItem extends React.Component {
     const link = `/stories/${this.props.story.id}`
     return (
       <div className="storiesIndexItem">
-        <Link to={link}><h4>{this.props.story.title}</h4></Link>
-        <p>{formBody}</p>
+        <img className="stories-index-img" src={this.props.story.story_img_url} />
+        <div className="index-title-body">
+          <Link to={link}><h4>{this.props.story.title}</h4></Link>
+          <p>{formBody}</p>
+        </div>
       </div>
     );
   }
