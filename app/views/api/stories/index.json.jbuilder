2cf241eee0158b226f646fd37c20e93a988dadc6
Eli <elicsiegel@gmail.com>
2017-08-24 21:59:38 -0400

    Style index and add seeds

--- app/views/api/stories/index.json.jbuilder
+++ app/views/api/stories/index.json.jbuilder
@@ -3,7 +3,9 @@
     json.id story.id
     json.title story.title
     json.body story.body
+    json.created_at story.created_at
     json.story_img_url asset_path(story.image.url)
     json.author story.author, :id, :username
+    json.author_img_url asset_path(story.author.image.url)
   end
 end
--- frontend/components/stories/stories_index_item.jsx
+++ frontend/components/stories/stories_index_item.jsx
@@ -3,8 +3,19 @@ import { Link } from 'react-router-dom';
 
 class StoriesIndexItem extends React.Component {
 
+  stylizeDate(created_at) {
+    const date = new Date(created_at);
+
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+      "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+    const dayOfMonth = date.getDate();
+
+    return `${months[month]} ${dayOfMonth}`;
+  }
+
   render() {
-    // debugger
     const formBody = this.props.story.body.slice(0, 100) + "..."
     const link = `/stories/${this.props.story.id}`
     return (
@@ -15,6 +26,13 @@ class StoriesIndexItem extends React.Component {
         <div className="index-title-body">
           <Link to={link}><h4>{this.props.story.title}</h4></Link>
           <p>{formBody}</p>
+          <div className="author-info-index">
+            <div className="author-index-image-div">
+              <img className="author-index-image" src={this.props.story.author_img_url} />
+            </div>
+            <p>{this.props.story.author.username}</p>
+            <p>{this.stylizeDate(this.props.story.created_at)}</p>
+          </div>
         </div>
       </div>
     );
