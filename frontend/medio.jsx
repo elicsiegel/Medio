64db6fa05e3a41f0933d9e8442ba8e08d7b44190
Eli <elicsiegel@gmail.com>
2017-11-21 12:13:52 -0500

    Clean up entry file

--- frontend/medio.jsx
+++ frontend/medio.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from './components/root'; 
+import Root from './components/root';
 import {login, signup, logout} from './actions/session_actions';
 
 import configureStore from './store/store';
 
-import { fetchStories } from './actions/story_actions'; 
-import { receiveComment, removeComment } from './actions/comment_actions'; 
+import { fetchStories } from './actions/story_actions';
+import { receiveComment, removeComment } from './actions/comment_actions';
 import {storyComments} from './reducers/selectors.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.storyComments = storyComments; 
-  window.receiveComment = receiveComment; 
-  window.removeComment = removeComment;
-  window.fetchStories = fetchStories;
   let store;
-  
+
   if (window.currentUser) {
     const preloadedState = { session: { currentUser: window.currentUser } };
     store = configureStore(preloadedState);
@@ -23,9 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore();
   }
-  window.store = store; 
-  window.dispatch = store.dispatch;
+  
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
 });
-
