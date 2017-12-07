5f4ef4bf6f04e4e672a773687a18422d1447dbb8
Eli <elicsiegel@gmail.com>
2017-12-07 14:22:01 -0500

    hide search bar on small screen

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
-import SessionFormContainer from './session/session_form_container'; 
+import SessionFormContainer from './session/session_form_container';
 import StoriesIndexContainer from './stories/stories_index_container';
 import StoryFormContainer from './stories/story_form_container';
-import StoryDetailContainer from './stories/story_detail_container'; 
+import StoryDetailContainer from './stories/story_detail_container';
 import BookmarkListContainer from './bookmarks/bookmark_list_container';
 import UserProfileContainer from './user/user_profile_container';
-import SearchContainer from './search/search_container'; 
+import SearchContainer from './search/search_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Route, Link, Switch } from 'react-router-dom';
 import ScrollToTop from './ScrollToTop';
